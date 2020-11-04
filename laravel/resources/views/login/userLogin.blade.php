1a7552b63aa253c7e143d27e87960d012d312965
finoana <finoanaandriatsilavoo@gmail.com>
2020-11-04 10:13:08 +0300

    modif page login

--- laravel/resources/views/login/userLogin.blade.php
+++ laravel/resources/views/login/userLogin.blade.php
@@ -19,21 +19,22 @@
     <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
         <div class="wrap-login100">
             <form class="login100-form validate-form">
+                @csrf
                 <span class="login100-form-logo">
                     <i class="zmdi zmdi-landscape"></i>
                 </span>
 
                 <span class="login100-form-title p-b-34 p-t-27">
-                    Log in
+                    SDA Social
                 </span>
 
                 <div class="wrap-input100 validate-input" data-validate = "Enter username">
-                    <input class="input100" type="text" name="username" placeholder="Username">
+                    <input class="input100" type="text" name="username" placeholder="nom utilisatuer">
                     <span class="focus-input100" data-placeholder="&#xf207;"></span>
                 </div>
 
                 <div class="wrap-input100 validate-input" data-validate="Enter password">
-                    <input class="input100" type="password" name="pass" placeholder="Password">
+                    <input class="input100" type="password" name="pass" placeholder="mot de pass">
                     <span class="focus-input100" data-placeholder="&#xf191;"></span>
                 </div>
 
